da1346e32aadfe14474451ca4d1362df0402d654
Awi <102220981+Awi22@users.noreply.github.com>
2025-03-10 11:05:07 +0800

    Tutor 2 : Blade Templating Engine - Style halaman dengan Tailwind - Instalasi Tailwind - Menggunakan Tailwind UI - Fix tamplate dan JS - Fix isi halaman

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -23,8 +23,8 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                                <a href="/blog" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Blog</a>
+                                <a href="/" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
+                                <a href="/blog" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Blog</a>
                                 <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
                                 <a href="contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
                             </div>
@@ -107,14 +107,14 @@
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Blog</h1>
             </div>
         </header>
 
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <!-- Your content -->
-                <p> Selamat datang di Home Page</p>
+                <p> Selamat datang di Blog</p>
             </div>
         </main>
     </div>
